fb48ba555dd455a3d0123b94946fb3d91e95da46
deep08 <deep.pate@edu.ece.fr>
2019-12-22 15:38:03 +0100

    All the views are done. Basic Routes as Well. Still missing the database to finish the log in/log out/ and register part

--- src/Routes/users.ts
+++ src/Routes/users.ts
@@ -2,13 +2,17 @@ import express from 'express'
 
 const router: express.Router = express.Router();
 
+// Render is to render a HTML Page 
+// And send put the message as it is 
+
 //Login Page 
 router.get('/login', (req:any, res:any)=>{
-    res.send('Login')
+    res.render('login')
 })
 
+//Register Page 
 router.get('/register', (req:any, res:any)=>{
-    res.send('Register')
+    res.render('register')
 })
 
 
